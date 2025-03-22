81a60d106d13fc124bc93a3c0a903a6cfd0443b8
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-03-22 23:57:03 +0200

    feat(i18n): add lang static generation and prop types

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next';
+import { dir } from 'i18next';
 
 import { AppProvider } from './app-provider';
+
+import { languages } from '@/i18n/settings';
+import { Locales } from '@/i18n/types';
 
 import '@/styles/globals.css';
 
@@ -13,13 +17,23 @@ export const metadata: Metadata = {
     "Mohamed Kamel's portfolio showcasing web development projects, skills, and contact information.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export async function generateStaticParams() {
+  return languages.map((lang) => ({ lang }));
+}
+
+type RootLayoutProps = Readonly<{
   children: React.ReactNode;
-}>) {
+  params: Promise<{ lang: Locales }>;
+}>;
+
+export default async function RootLayout({
+  children,
+  params,
+}: RootLayoutProps) {
+  const { lang } = await params;
+
   return (
-    <html lang="en">
+    <html lang={lang} dir={dir(lang)}>
       <body>
         <AppProvider>{children}</AppProvider>
       </body>
