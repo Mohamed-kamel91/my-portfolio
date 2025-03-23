58b65963b032b6e1a9b04bf80ac0a9d1b0136f56
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-03-23 02:01:51 +0200

    feat(i18n): add getOptions for i18n configuration

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -2,3 +2,17 @@ export const fallbackLng = 'en';
 export const languages = [fallbackLng, 'ar'] as const;
 export const defaultNS = 'common';
 export const cookieName = 'i18next';
+
+export function getOptions(
+  lng = fallbackLng,
+  ns: string | string[] = defaultNS,
+) {
+  return {
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns,
+  };
+}
