9692d29016ce13bca5f385e73cc79938982e2d29
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-03-27 22:55:11 +0200

    feat(i18n): integrate LanguageProvider into AppProvider

--- src/app/[lang]/app-provider.tsx
+++ src/app/[lang]/app-provider.tsx
@@ -1,9 +1,28 @@
+'use client';
+
+import { CookiesProvider } from 'react-cookie';
+
 import { PreloadResources } from './preload-resources';
+import { LanguageProvider } from '@/features/language';
+
+import { Locales } from '@/i18n/types';
 
 type AppProviderProps = {
+  lang: Locales;
   children: React.ReactNode;
 };
 
-export const AppProvider = ({ children }: AppProviderProps) => {
-  return <PreloadResources>{children}</PreloadResources>;
+export const AppProvider = ({
+  lang,
+  children,
+}: AppProviderProps) => {
+  return (
+    <PreloadResources>
+      <CookiesProvider>
+        <LanguageProvider lang={lang}>
+          {children}
+        </LanguageProvider>
+      </CookiesProvider>
+    </PreloadResources>
+  );
 };
