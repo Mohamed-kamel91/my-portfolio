5aeba8e68063cc2007d5fb1b9b6e0ec00ad4fb9c
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-04-02 17:27:45 +0200

    chore(theme): mock window matchmedia

--- src/test/setup-tests.ts
+++ src/test/setup-tests.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom/vitest';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  configurable: true,
+  value: vi.fn((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
