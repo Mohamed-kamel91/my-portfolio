0edb473071565c36cab25711bf51325f83903221
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-04-06 18:17:57 +0200

    refactor(hook): wrap tests with describe() and change test prefix to it()

--- src/hooks/useDisclosure.test.ts
+++ src/hooks/useDisclosure.test.ts
@@ -1,54 +1,56 @@
 import { act, renderHook } from '@/test/test-utils';
 import { useDisclosure } from './useDisclosure';
 
-test('should open the state', () => {
-  const { result } = renderHook(() => useDisclosure());
+describe('useDisclosure', () => {
+  it('should open the state', () => {
+    const { result } = renderHook(() => useDisclosure());
 
-  expect(result.current.isOpen).toBe(false);
+    expect(result.current.isOpen).toBe(false);
 
-  act(() => {
-    result.current.open();
+    act(() => {
+      result.current.open();
+    });
+
+    expect(result.current.isOpen).toBe(true);
   });
 
-  expect(result.current.isOpen).toBe(true);
-});
+  it('should close the state', () => {
+    const { result } = renderHook(() => useDisclosure());
 
-test('should close the state', () => {
-  const { result } = renderHook(() => useDisclosure());
+    expect(result.current.isOpen).toBe(false);
 
-  expect(result.current.isOpen).toBe(false);
+    act(() => {
+      result.current.close();
+    });
 
-  act(() => {
-    result.current.close();
+    expect(result.current.isOpen).toBe(false);
   });
 
-  expect(result.current.isOpen).toBe(false);
-});
+  it('should toggle the state', () => {
+    const { result } = renderHook(() => useDisclosure());
 
-test('should toggle the state', () => {
-  const { result } = renderHook(() => useDisclosure());
+    expect(result.current.isOpen).toBe(false);
 
-  expect(result.current.isOpen).toBe(false);
+    act(() => result.current.toggle());
 
-  act(() => result.current.toggle());
+    expect(result.current.isOpen).toBe(true);
 
-  expect(result.current.isOpen).toBe(true);
+    act(() => {
+      result.current.toggle();
+    });
 
-  act(() => {
-    result.current.toggle();
+    expect(result.current.isOpen).toBe(false);
   });
 
-  expect(result.current.isOpen).toBe(false);
-});
+  it('should define initial state', () => {
+    const { result } = renderHook(() => useDisclosure(true));
 
-test('should define initial state', () => {
-  const { result } = renderHook(() => useDisclosure(true));
+    expect(result.current.isOpen).toBe(true);
 
-  expect(result.current.isOpen).toBe(true);
+    act(() => {
+      result.current.toggle();
+    });
 
-  act(() => {
-    result.current.toggle();
+    expect(result.current.isOpen).toBe(false);
   });
-
-  expect(result.current.isOpen).toBe(false);
 });
