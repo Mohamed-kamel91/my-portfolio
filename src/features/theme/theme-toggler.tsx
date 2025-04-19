310f07fc367a4b92a6029b7a3f73a2d138ed13e8
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-04-19 23:58:26 +0200

    refactor(theme): refactor theme toggler

--- src/features/theme/theme-toggler.tsx
+++ src/features/theme/theme-toggler.tsx
@@ -2,15 +2,11 @@
 
 import { Moon, Sun } from 'lucide-react';
 
-import {
-  IconButton,
-  IconButtonProps,
-} from '@/components/ui/buttons';
+import { IconButton, IconButtonProps } from '@/components/ui/buttons';
 
+import { useI18n } from '@/i18n/client';
 import { useMounted } from '@/hooks/useMounted';
-import { useLanguage } from '../language';
 import { useThemeToggler } from './useThemeToggler';
-import { useTranslation } from '@/i18n/client';
 
 type ThemeTogglerProps = IconButtonProps<'button'>;
 
@@ -18,17 +14,19 @@ export const ThemeToggler = ({
   className,
   ...props
 }: ThemeTogglerProps) => {
-  const { nextTheme, isDark, handleThemeToggle } =
-    useThemeToggler();
-  const { currentLang } = useLanguage();
-  const { t } = useTranslation(currentLang);
+  const { nextTheme, isDark, handleThemeToggle } = useThemeToggler();
+  const { t } = useI18n();
   const isMounted = useMounted();
 
   if (!isMounted) {
     return null;
   }
 
-  const actionLabel = t('theme.label', {
+  const themetitle = t('theme.title', {
+    theme: t(`theme.modes.${nextTheme}`),
+  });
+
+  const themeLabel = t('theme.label', {
     theme: t(`theme.modes.${nextTheme}`),
   });
 
@@ -36,8 +34,8 @@ export const ThemeToggler = ({
     <IconButton
       className={className}
       variant="secondary"
-      title={`${nextTheme} mode`}
-      aria-label={actionLabel}
+      title={themetitle}
+      aria-label={themeLabel}
       aria-pressed={isDark}
       data-testid="theme-toggle-btn"
       onClick={handleThemeToggle}
