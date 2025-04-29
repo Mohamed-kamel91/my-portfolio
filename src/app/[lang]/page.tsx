461450abecc6dd571b28aa7d314a61bef8d7dea6
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-04-29 04:35:31 +0300

    feat(home): modify home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,5 +1,18 @@
-import { MainLayout } from '@/app/[lang]/_components/main-layout';
+import { MainLayout } from '@/components/layouts/main-layout';
+import { HeroHeader } from './_components/hero-header';
+import { About } from './_components/about';
+import { Experience } from './_components/experience';
+import { Projects } from './_components/projects';
+import { Contact } from './_components/contact';
 
 export default function Home() {
-  return <MainLayout>a</MainLayout>;
+  return (
+    <MainLayout>
+      <HeroHeader />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </MainLayout>
+  );
 }
