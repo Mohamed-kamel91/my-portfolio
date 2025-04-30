e58bde7ece711b2e4c16fb6d45048eaf1862be3b
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-04-30 03:40:03 +0300

    refactor(layout): refactor main layout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,17 +1,9 @@
 'use client';
 
-import { Container } from '@/components/ui/containers';
-import { Stack } from '@/components/ui/stack';
-import { MatchMedia } from '@/components/ui/match-media';
-import { MobileLogo } from '../navigation/mobile-logo';
-import { MobileNavigation } from '../navigation/mobile-navigation';
-import { MainNavigation } from '../navigation/main-navigation';
-import { NavActions } from '../navigation/nav-actions';
+import { Header } from './header';
+import { Footer } from './footer';
 
 import { useMounted } from '@/hooks';
-
-import { cn } from '@/utils/cn';
-import { socials } from '@/constants/socials';
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -24,51 +16,9 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
 
   return (
     <div>
-      <header className="py-10 md:py-14">
-        <Container className="max-w-6xl">
-          <Stack align="center" justify="between" gap={4}>
-            <MatchMedia size="md" perspective="max">
-              <MobileLogo />
-              <MobileNavigation />
-            </MatchMedia>
-
-            <MatchMedia size="md">
-              <MainNavigation />
-              <NavActions />
-            </MatchMedia>
-          </Stack>
-        </Container>
-      </header>
-
+      <Header />
       <main>{children}</main>
-
-      <footer>
-        <Container className="max-w-6xl">
-          <Stack
-            justify="between"
-            align="start"
-            gap={4}
-            className={cn(
-              'pt-5 pb-10 md:pb-14',
-              'border-border border-t',
-            )}
-          >
-            <p>&copy; M.K. {new Date().getFullYear()}</p>
-            <Stack gap={4} align="center">
-              {socials.map((social) => (
-                <a
-                  key={social.name}
-                  href={social.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {social.name}
-                </a>
-              ))}
-            </Stack>
-          </Stack>
-        </Container>
-      </footer>
+      <Footer />
     </div>
   );
 };
