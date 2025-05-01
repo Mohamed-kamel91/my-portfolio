491db9f671947436552d3e667eee1b6afa306f13
Mohamed Kamel <71556614+Mohamed-kamel91@users.noreply.github.com>
2025-05-02 01:31:52 +0300

    feat(page/projects): add projects route

---
+++ src/app/[lang]/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ProjectsContent } from './_components/projects-content';
+
+export default function ProjectsPage() {
+  return (
+    <div className="pt-5 pb-20 md:pb-30">
+      <ProjectsContent />
+    </div>
+  );
+}
